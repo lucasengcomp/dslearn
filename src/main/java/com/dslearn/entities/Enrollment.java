1119257def85fcfa126104bd74042e3f52ef9a97
Lucas Galvao <lucas.engcomp@outlook.com>
2023-06-27 05:12:41 -0300

    :sparkles: `feat` Add Enrollment, EnrollmentPK, relation composite key with EnrollmentPK

---
+++ src/main/java/com/dslearn/entities/Enrollment.java
@@ -0,0 +1,96 @@
+package com.dslearn.entities;
+
+import com.dslearn.entities.pk.EnrollmentPK;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private EnrollmentPK id = new EnrollmentPK();
+    private Instant enrollMoment;
+    private Instant refundMoment;
+    private boolean available;
+    private boolean onlyUpdate;
+
+    public Enrollment() {
+    }
+
+    public Enrollment(User user, Offer offer, Instant enrollMoment,
+                      Instant refundMoment, boolean available, boolean onlyUpdate) {
+        id.setUser(user);
+        id.setOffer(offer);
+        this.enrollMoment = enrollMoment;
+        this.refundMoment = refundMoment;
+        this.available = available;
+        this.onlyUpdate = onlyUpdate;
+    }
+
+    public User getStudent() {
+        return id.getUser();
+    }
+
+    public void setStudent(User user) {
+        id.setUser(user);
+    }
+
+    public Offer getOffer() {
+        return id.getOffer();
+    }
+
+    public void setOffer(Offer offer) {
+        id.setOffer(offer);
+    }
+
+    public Instant getEnrollMoment() {
+        return enrollMoment;
+    }
+
+    public void setEnrollMoment(Instant enrollMoment) {
+        this.enrollMoment = enrollMoment;
+    }
+
+    public Instant getRefundMoment() {
+        return refundMoment;
+    }
+
+    public void setRefundMoment(Instant refundMoment) {
+        this.refundMoment = refundMoment;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public boolean isOnlyUpdate() {
+        return onlyUpdate;
+    }
+
+    public void setOnlyUpdate(boolean onlyUpdate) {
+        this.onlyUpdate = onlyUpdate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Enrollment that = (Enrollment) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
